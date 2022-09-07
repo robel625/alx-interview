8c6ed1fb27b50165947e6a2d0c941303ef3fa283
robel625 <76595851+robel625@users.noreply.github.com>
2022-09-07 19:19:35 +0200

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,96 +1,64 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@robel625 
-Mahiuha
-/
-alx-interview
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-interview/0x03-log_parsing/0-stats.py /
-@Mahiuha
-Mahiuha reads stdin line by line and computes metrics
-Latest commit c7d6e82 on Apr 28
- History
- 1 contributor
-Executable File  54 lines (42 sloc)  1.17 KB
-
 #!/usr/bin/python3
-
+"""
+Module 0-stats
+script that reads stdin line by line and computes metrics
+"""
 import sys
-
-
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
-
+import re
 
 total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
 
-try:
-    for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
+# sample_line = '36.196.190.72 - [2022-04-26 07:59:21.687812] '\
+#        '"GET /projects/260 HTTP/1.1" 500 878'
 
-        if len(parsed_line) > 2:
-            counter += 1
+pattern = r'^([\d]{1,3}\.){3}([\d]{1,3})'
+pattern += r'( - )(\[[\d]{4}-[\d]{2}-[\d]{2}'
+pattern += r' [\d]{2}:[\d]{2}:[\d]{2}\.[\d]{1,}\])'
+pattern += r'( "GET \/projects\/260 HTTP\/1\.1") '
+pattern += r'([\d]{3}) ([\d]{1,4})$'
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
 
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
+codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
+no_of_lines = 0
 
-finally:
-    print_msg(dict_sc, total_file_size)
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-You have unread notifications
+
+def is_line_valid(line: str) -> bool:
+    result = re.match(pattern, line)
+    if result:
+        return True
+    return False
+
+
+def generate_statistics(line: str) -> None:
+    global total_file_size
+    chars = line.split(' ')
+
+    file_size = int(chars[-1].replace('\n', ''))
+    try:
+        status_code = int(chars[-2])
+        if status_code in codes:
+            codes[status_code] += 1
+    except ValueError:
+        pass
+
+    total_file_size += file_size
+
+
+def print_statistics() -> None:
+    print('File size: {}'.format(total_file_size))
+    for key, value in sorted(codes.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
+
+if __name__ == '__main__':
+    try:
+        for i, line in enumerate(sys.stdin, 1):
+            # generate statistics only for a valid log
+            if is_line_valid(line):
+                generate_statistics(line)
+            if not i % 10:
+                print_statistics()
+    except KeyboardInterrupt:
+        print_statistics()
+        raise
